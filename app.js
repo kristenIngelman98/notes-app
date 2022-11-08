24889cca039a2becc1d91f50a3a322eccb15bba7
Kristen <kristeningelman@hotmail.com>
2022-11-08 09:59:46 -0800

    practice with storing data with JSON

--- app.js
+++ app.js
@@ -14,8 +14,19 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title'
+        },
+        body: {
+            describe: 'Note description',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
     }
 })
 
@@ -47,7 +58,8 @@ yargs.command({
 })
 // const command = process.argv[2]
 
-console.log(process.argv)
-console.log(yargs.argv)
+// console.log(process.argv)
+// console.log(yargs.argv)
+yargs.parse()
 
 
